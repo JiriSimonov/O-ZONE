f5be04f1194678286e832c451f2a4504cc18ead3
Jiri S <jirisimonov@gmail.com>
2022-05-20 11:39:55 +0300

    feat: add post method for cart items

--- src/modules/postData.js
+++ src/modules/postData.js
@@ -1,18 +1,12 @@
-const postData = () => {
-    fetch('https://o-zone-a3d75-default-rtdb.firebaseio.com/goods.json', {
+const postData = (cart) => {
+    return fetch('https://jsonplaceholder.typicode.com/posts', {
         method: 'POST',
-        body: JSON.stringify({
-            title: '1',
-            price: 3000,
-            sale: false,
-            img: '',
-            category: "Игры и софт"
-        }),
+        body: JSON.stringify(cart),
         headers: {
             'Content-type': 'application/json; charset=UTF-8',
         },
     })
-        .then(res => res.json())
+    .then(res => res.json())
 }
 
 export default postData
